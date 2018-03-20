433ed3c95f32fa2be20b67697ce2038760310e17
Kangyun Wang <wky@087.167.028.145.hva.nl>
2018-03-20 12:10:43 +0100

    Add nodemon

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,7 @@
 module.exports = {
 	 "extends": "standard",
+	 "rules": {
+		 "semi": "always",
+		 "indent": ["error", "tab"]
+	 }
 };
--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-var express = require('express')
+var express = require('express');
 
-var app = express()
+var app = express();
 
 // Require the dotenv file
-require('dotenv').config({ path: './vars.env' })
+require('dotenv').config({ path: './vars.env' });
 
-app.use(express.static('public'))
-app.set('view engine', 'pug')
-app.set('views', 'views')
+app.use(express.static('public'));
+app.set('view engine', 'pug');
+app.set('views', 'views');
 
 app.get('/', function (req, res) {
-  res.send('Hello Mate!')
+  res.send('Hello Mate!');
 })
 
 app.listen(3500, function () {
-  console.log('server is running on port 3500')
+  console.log('server is running on port 3500');
 })
