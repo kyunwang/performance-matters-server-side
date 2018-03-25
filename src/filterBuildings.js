9a48bca7e43e33c0513382c4ccdf11b71117c471
Kangyun Wang <wky@kangyuns-mbp.home>
2018-03-25 17:41:28 +0200

    Corrected the commonjs export

--- src/filterBuildings.js
+++ src/filterBuildings.js
@@ -1,32 +1,32 @@
-module.exports = (function () {
-	var buildingList = document.getElementsByClassName('building');
-	var filterLabels = document.getElementsByName('key');
+var buildingList = document.getElementsByClassName('building');
+var filterLabels = document.getElementsByName('key');
 
-	// Object to contain the filterstates
-	var activeFilters = {};
+// Object to contain the filterstates
+var activeFilters = {};
 
-	for (var index = 0; index < filterLabels.length; index++) {
-		// Setting the initials filter states
-		activeFilters[filterLabels[index].value] = filterLabels[index].checked;
+for (var index = 0; index < filterLabels.length; index++) {
+	// Setting the initials filter states
+	activeFilters[filterLabels[index].value] = filterLabels[index].checked;
 
-		filterLabels[index].addEventListener('click', function () {
-			// Change the filterstate on click and filter the buildings
-			activeFilters[this.value] = this.checked;
-			filterBuildings();
-		});
-	}
+	filterLabels[index].addEventListener('click', function () {
+		// Change the filterstate on click and filter the buildings
+		activeFilters[this.value] = this.checked;
+		filterBuildings();
+	});
+}
 
-	function filterBuildings () {
-		for (var indexB = 0; indexB < buildingList.length; indexB++) {
-			var buildingType = buildingList[indexB].dataset.type;
-			var building = buildingList[indexB];
+function filterBuildings () {
+	for (var indexB = 0; indexB < buildingList.length; indexB++) {
+		var buildingType = buildingList[indexB].dataset.type;
+		var building = buildingList[indexB];
 
-			if (activeFilters[buildingType]) {
-				// Not using classlist for support atm
-				building.style.display = 'block';
-			} else {
-				building.style.display = 'none';
-			}
+		if (activeFilters[buildingType]) {
+			// Not using classlist for support atm
+			building.style.display = 'block';
+		} else {
+			building.style.display = 'none';
 		}
 	}
-})();
+}
+
+module.exports = filterBuildings;
